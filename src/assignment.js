1b249ef43de25ab929e46b74c6d235c30f3453d3
Chibuike Ukandu <67115735+Jay035@users.noreply.github.com>
2022-07-09 10:51:43 +0000

    attempt on challenges 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // return arrayOfNumbers.length;
+  let sum = 0;
+  for(let number of arrayOfNumbers){
+    sum += number;
+  }
+  return sum;
+    
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,13 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  // return arrayOfNumbers.length;
+  const even = arrayOfNumbers.filter(number => {
+    return number % 2 === 0;
+  })
+  return even.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
